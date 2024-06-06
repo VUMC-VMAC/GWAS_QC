31012ce37db6fb53d71f819180670b03260f3f35
ermahoney23 <emily.r.mahoney@vumc.org>
2024-06-06 12:57:04 -0500

    move check for imputation results so that it only happens if the first filters are being done

--- gwas_qc_postimputation_part1.sh
+++ gwas_qc_postimputation_part1.sh
@@ -10,7 +10,7 @@ display_usage() {
 This script will unzip the imputation results (assuming password is saved in pass.txt in the same folder as the imputation results files and will perform the first steps of standard post-imputation QC for our common variant pipeline. This includes filtering for R2, removing multi-allelic variants and filtering out variants for low MAF. Then, ancestry and PCs will be estimated using pre-calculated weights. The output of this script will include filtered genotypes, plots of predicted PCs, and a file including ancestral group estimations. Please check these outputs, make any filtering decisions necessary and proceed with the part 2 post-imputation script.
 
 Usage:
-SCRIPTNAME.sh -o [output_stem] -i [imputation_results_folder] -r [race_file] -f [sex_file] -s [snp_names_file] -g [preimputation_geno] -w [snpweights_file] -z -x -c
+SCRIPTNAME.sh -o [output_stem] -i [imputation_results_folder] -r [race_file] -f [sex_file] -s [snp_names_file] -g [preimputation_geno] -w [snpweights_file] -z -x -m [plink_memory_flag] -c
 
 output_stem = the beginning part of all QC'ed files including the full path to the folder in which they should be created
 
@@ -24,6 +24,8 @@ preimputation_geno = the full path and stem to the cleaned final pre-imputation 
 
 snpweights_file = the full path and name of the file containing pre-calculated weights from which to calculate ancestral estimates; note that there must be a corresponding file with the same stem ending in *_snps.txt which contains a list of variants to subset the current set to before calculating weights
 
+plink_memory_limit (optional) = argument indicating the memory limit for plink to use rather than the default of half the RAM. This is useful for running this step of QC locally.
+
 -z indicates that the imputation results will need to be unzipped.
 -x indicates to skip the first R2<0.8 and multi-allelic variant filtering of the individual chr files. This comes in handy if there were an issue with the next step(s) because this first step is the longest.
 -c will skip the clean-up at the end of the script which removes intermediate *.bed files.
@@ -35,7 +37,7 @@ snpweights_file = the full path and name of the file containing pre-calculated w
 do_unzip='false'
 skip_first_filters='false'
 skip_cleanup='false'
-while getopts 'o:i:f:s:g:w:zxch' flag; do
+while getopts 'o:i:f:s:g:w:zxcm:h' flag; do
   case "${flag}" in
     o) output_stem="${OPTARG}" ;;
     i) imputation_results_folder="${OPTARG}" ;;
@@ -46,6 +48,7 @@ while getopts 'o:i:f:s:g:w:zxch' flag; do
     w) snpweights_file="${OPTARG}" ;;
     x) skip_first_filters='true' ;;
     c) skip_cleanup='true' ;;
+    m) plink_memory_limit="${OPTARG}";;
     h) display_usage ; exit ;;
     \?|*) display_usage
        exit 1;;
@@ -87,6 +90,12 @@ then
     exit 1
 fi
 
+if [ "$plink_memory_limit" ];
+then 
+    printf "Memory limit for plink calls: $plink_memory_limit \n"
+    plink_memory_limit=$( echo "--memory $plink_memory_limit" )
+fi
+
 #check to make sure this is being run in the scripts folder (checking if necessary script is present)
 if test ! -f get_related_ids.R ;
 then
@@ -108,35 +117,38 @@ output_folder=${output_stem%/*}
 
 ################# Start the post-imputation QC ######################
 
-if [ "$do_unzip" = 'true' ];
-then
-    printf "Step 1 : Unzipping imputation results\n\n"
-
-    #check to make sure password file is present
-    if [ ! -f "${imputation_results_folder}/pass.txt" ];
-    then
-	printf "Imputation password file not found! Please save the password to unzip imputation results (found in the email notification from the imputation server of the job's completion) to ${imputation_results_folder}/pass.txt\n"
-	exit 1
-    fi
-
-    #unzip imputation results using password
-    for i in $( ls ${imputation_results_folder}/*.zip );
-    do
-	unzip -P $( cat ${imputation_results_folder}/pass.txt ) $i -d $imputation_results_folder
-    done
-else
-    #make sure the imputation results are actually unzipped
-    if test ! -f ${imputation_results_folder}/chr1.dose.vcf.gz ;
-    then 
-	printf "The -z was not specified but cannot find the imputation results (ie ${imputation_results_folder}/chr1.dose.vcf.gz for each chromosome )! Please check whether imputation results have been unzipped. If they haven't, make sure the decryption password is saved in ${imputation_results_folder}/pass.txt and specify the -z flag.\n"
-	exit 1
-    else
-	printf "Step 1 (unzipping the imputation results) is already complete. Proceeding the step 2...\n\n"
-    fi
-fi
 
 if [ $skip_first_filters = 'false' ];
 then
+    
+    # first, unzip the imputation results if needed. 
+    if [ "$do_unzip" = 'true' ];
+    then
+	printf "Step 1 : Unzipping imputation results\n\n"
+
+	#check to make sure password file is present
+	if [ ! -f "${imputation_results_folder}/pass.txt" ];
+	then
+	    printf "Imputation password file not found! Please save the password to unzip imputation results (found in the email notification from the imputation server of the job's completion) to ${imputation_results_folder}/pass.txt\n"
+	    exit 1
+	fi
+
+	#unzip imputation results using password
+	for i in $( ls ${imputation_results_folder}/*.zip );
+	do
+	    unzip -P $( cat ${imputation_results_folder}/pass.txt ) $i -d $imputation_results_folder
+	done
+    else
+	#make sure the imputation results are actually unzipped
+	if test ! -f ${imputation_results_folder}/chr1.dose.vcf.gz ;
+	then 
+	    printf "The -z was not specified but cannot find the imputation results (ie ${imputation_results_folder}/chr1.dose.vcf.gz for each chromosome )! Please check whether imputation results have been unzipped. If they haven't, make sure the decryption password is saved in ${imputation_results_folder}/pass.txt and specify the -z flag.\n"
+	    exit 1
+	else
+	    printf "Step 1 (unzipping the imputation results) is already complete. Proceeding the step 2...\n\n"
+	fi
+    fi
+
     # before the loop, initialize the file for merging each chr together
     echo "" | tee ${output_stem}_merge_list.txt
 
@@ -144,15 +156,15 @@ then
     printf "Step 2 : Filtering imputation results for R2<0.8 and multi-allelic variants\n"
     for i in $(seq 1 22); do
 	#restrict to variants with R2>=0.80
-	plink2 --vcf ${imputation_results_folder}/chr${i}.dose.vcf.gz --const-fid 0 --exclude-if-info "R2<0.8" --make-bed --out ${output_stem}_chr${i}_temp > /dev/null
+	plink2 --vcf ${imputation_results_folder}/chr${i}.dose.vcf.gz --const-fid 0 --exclude-if-info "R2<0.8" --make-bed --out ${output_stem}_chr${i}_temp $plink_memory_limit > /dev/null
 	
 	#get list of variants which have the same position (by base pair since this is just 1 chromosome) and remove
 	dup_pos=$( awk '{ print $4 }' ${output_stem}_chr${i}_temp.bim | uniq -d )
 	for j in $dup_pos ; do grep -w $j ${output_stem}_chr${i}_temp.bim | awk '{ print $2 }' >> ${output_stem}_chr${i}.dups ; done
-	plink2 --bfile ${output_stem}_chr${i}_temp --exclude ${output_stem}_chr${i}.dups --make-bed --out ${output_stem}_chr${i}_temp_nodups > /dev/null
+	plink2 --bfile ${output_stem}_chr${i}_temp --exclude ${output_stem}_chr${i}.dups --make-bed --out ${output_stem}_chr${i}_temp_nodups $plink_memory_limit > /dev/null
 	
 	#update variant names with rs numbers
-	plink2 --bfile ${output_stem}_chr${i}_temp_nodups --update-name ${snp_names_file}_chr${i}.txt --make-bed --out ${output_stem}_chr${i}_temp_nodups_names > /dev/null
+	plink2 --bfile ${output_stem}_chr${i}_temp_nodups --update-name ${snp_names_file}_chr${i}.txt --make-bed --out ${output_stem}_chr${i}_temp_nodups_names $plink_memory_limit > /dev/null
 	
 	#check the length of variant IDs
 	awk 'length($2)>30{ print $2" "$1"_"$4 }' ${output_stem}_chr${i}_temp_nodups_names.bim > ${output_stem}_chr${i}_temp_nodups_names_shortids.txt
@@ -160,14 +172,13 @@ then
 	# either way, add the appropriate file name to the merge-list
 	if [ $(ls ${output_stem}_chr${i}_temp_nodups_names_shortids.txt) ] ;
 	then
-	    plink --bfile ${output_stem}_chr${i}_temp_nodups_names --update-name ${output_stem}_chr${i}_temp_nodups_names_shortids.txt --make-bed --out ${output_stem}_chr${i}_temp_nodups_names_shorterids --memory 15000 > /dev/null ; 
+	    plink --bfile ${output_stem}_chr${i}_temp_nodups_names --update-name ${output_stem}_chr${i}_temp_nodups_names_shortids.txt --make-bed --out ${output_stem}_chr${i}_temp_nodups_names_shorterids $plink_memory_limit > /dev/null ; 
 	    printf "Updating $( grep 'updated' ${output_stem}_chr${i}_temp_nodups_names_shorterids.log | awk '{ print $2 }') too-long IDs from chr ${i}...\n" ; 
 	    printf "${output_stem}_chr${i}_temp_nodups_names_shorterids\n" >> ${output_stem}_merge_list.txt
 	else
 	    printf "IDs in chr ${i} ok.\n"
 	    printf "${output_stem}_chr${i}_temp_nodups_names\n" >> ${output_stem}_merge_list.txt ;
 	fi
-
 
     done
 
@@ -182,11 +193,14 @@ then
 
     #merge individual chromosome files to be one large file
     output=${output_stem}
-    plink --merge-list ${output_stem}_merge_list.txt --make-bed --out $output > /dev/null
+    plink --merge-list ${output_stem}_merge_list.txt --make-bed --out $output $plink_memory_limit > /dev/null
     grep 'pass filters and QC' ${output}.log
 else
     output=$output_stem
-    printf "Skipping the conversion and filtering of the individual chromosome because the -x flag was supplied! Picking up at updating sample IDs and sex in the merged file. Assuming the merged file stem is $output\n"
+    # check for the presence of the merged file with the supplied name and fail if not present
+    if [ ! -f ${output_stem}.bed ]; then exit 1 ; fi
+    # if present, log skipping the first step
+    printf "Skipping the unzipping, conversion, and filtering of the individual chromosome because the -x flag was supplied! Picking up at updating sample IDs and sex in the merged file. Assuming the merged file stem is $output\n"
 fi
 
 #### Updating person ids and sex ####
@@ -194,14 +208,14 @@ fi
 output_last=$output
 output=${output}_IDs
 awk '{ print "0 "$1"_"$2" "$1" "$2 }' $sex_file > ${output_last}_update_ids.txt
-plink --bfile $output_last --update-ids ${output_last}_update_ids.txt --make-bed --out $output > /dev/null
+plink --bfile $output_last --update-ids ${output_last}_update_ids.txt --make-bed --out $output $plink_memory_limit > /dev/null
 tmp=$( grep "people updated" ${output}.log | awk '{ print $2 }' )
 printf "$tmp people whose ids were able to be updated using the sex file.\n"
 
 #update SNP names and add sex back into fam file
 output_last=$output
 output=${output}_sex
-plink --bfile ${output_last} --update-sex ${sex_file} --make-bed --out ${output} > /dev/null
+plink --bfile ${output_last} --update-sex ${sex_file} --make-bed --out ${output} $plink_memory_limit > /dev/null
 
 ###### merge back in genotypes #####
 
@@ -213,11 +227,11 @@ awk '{ print $2 }' ${preimputation_geno}.bim > ${output_folder}/genotyped_varian
 # remove variants for which there are genotypes from the bim file
 output_last=$output
 output=${output}_nogeno
-plink --bfile ${output_last} --exclude ${output_folder}/genotyped_variants.txt --make-bed --out $output > /dev/null
+plink --bfile ${output_last} --exclude ${output_folder}/genotyped_variants.txt --make-bed --out $output $plink_memory_limit > /dev/null
 printf "$(grep -e "variants remaining" ${output}.log | awk '{ print $2 }') variants remaining after removing genotyped variants from imputation results.\n"
 
 # merge the genotyped and imputed data
-plink --bfile ${output} --bmerge ${preimputation_geno} --make-bed --out ${output}_merged > /dev/null
+plink --bfile ${output} --bmerge ${preimputation_geno} --make-bed --out ${output}_merged $plink_memory_limit > /dev/null
 
 #check for complete sample overlap in the log and throw an error if there is not complete overlap
 new_samples=$( grep "base dataset" ${output}_merged.log | awk 'NR==1{ print $3 }' )
@@ -233,9 +247,9 @@ if [ ! -z "$sameposwarnings" ] ;
 then 
     printf "Getting same position warnings. Removing those variants from the imputed dataset and re-attempting merge.\n"
     grep "Warning: Variants" ${output}_merged.log | awk  '{ print $3"\n"$5 }' | sed -e "s/'//g" >  ${output_folder}/genotyped_variants_sameposwarnings.txt
-    plink --bfile ${output} --exclude ${output_folder}/genotyped_variants_sameposwarnings.txt --make-bed --out ${output}2 > /dev/null
+    plink --bfile ${output} --exclude ${output_folder}/genotyped_variants_sameposwarnings.txt --make-bed --out ${output}2 $plink_memory_limit > /dev/null
     printf "$(grep -e "variants remaining" ${output}.log | awk '{ print $2 }' ) variants remaining after removing genotyped variants from imputation results based on position.\n"
-    plink --bfile ${output}2 --bmerge ${preimputation_geno} --make-bed --out ${output}_merged > /dev/null
+    plink --bfile ${output}2 --bmerge ${preimputation_geno} --make-bed --out ${output}_merged $plink_memory_limit > /dev/null
     
     #check for more warnings
     sameposwarnings=$( grep "Warning: Variants" ${output}_merged.log | head -n1 )
@@ -252,17 +266,17 @@ printf "Output file: $output"
 # Calculate SNPWeights
 printf "\nStep 5: Calculate inferred ancestry.\nFirst, subsetting to overlap with pre-calculated weights and removing non-ATGC variants.\n"
 ## Filter down to the variants in 1000G
-plink --bfile ${output} --extract ${snpweights_file}_snp.txt --make-bed --out ${output}_overlap > /dev/null 
+plink --bfile ${output} --extract ${snpweights_file}_snp.txt --make-bed --out ${output}_overlap $plink_memory_limit > /dev/null 
 grep -e 'pass filters and QC' ${output}.log
 
 ## remove non-ATGC variants
-plink --bfile ${output}_overlap --snps-only 'just-acgt' --make-bed --out ${output}_overlap_acgt > /dev/null
+plink --bfile ${output}_overlap --snps-only 'just-acgt' --make-bed --out ${output}_overlap_acgt $plink_memory_limit > /dev/null
 grep -e 'pass filters and QC' ${output}.log
 
 ## Prune 
 printf "Pruning...\n"
-plink --bfile ${output}_overlap_acgt --indep-pairwise 200 100 0.2 --allow-no-sex --out ${output}_overlap_acgt_prune > /dev/null
-plink --bfile ${output}_overlap_acgt --output-missing-phenotype 1 --extract ${output}_overlap_acgt_prune.prune.in --make-bed --out ${output}_overlap_acgt_pruned > /dev/null
+plink --bfile ${output}_overlap_acgt --indep-pairwise 200 100 0.2 --allow-no-sex --out ${output}_overlap_acgt_prune $plink_memory_limit > /dev/null
+plink --bfile ${output}_overlap_acgt --output-missing-phenotype 1 --extract ${output}_overlap_acgt_prune.prune.in --make-bed --out ${output}_overlap_acgt_pruned $plink_memory_limit > /dev/null
 rm ${output}_overlap_acgt_prune.*
 printf "$( wc -l < ${output}_overlap_acgt_pruned.bim ) variants out of $( wc -l < ${output}_overlap_acgt.bim ) left after pruning.\n"
 
